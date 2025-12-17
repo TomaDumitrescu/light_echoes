1c34d8f74bc04b912ee7e61df177913bc4257597
BLemonG <leon.mueller@go4more.de>
2025-12-17 18:39:41 +0100

    Update menu.gd

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -7,7 +7,5 @@ func _on_play_button_button_down():
 	AudioManager.play_sfx(AudioManager.click_sfx)
 	get_tree().change_scene_to_file("res://scenes/main.tscn")
 	
-
-
 func _on_play_button_mouse_entered() -> void:
 	AudioManager.play_hover_sfx()
