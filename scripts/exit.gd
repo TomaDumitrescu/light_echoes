54fa9ff3c7550248906ec5aef133fe3ac5972326
Roberto <IvanFernandez_RobertoMatis@outlook.es>
2025-12-18 16:04:59 +0100

    Changed Exit

--- scripts/exit.gd
+++ scripts/exit.gd
@@ -1,5 +1,10 @@
 extends Area2D
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
+
+func _ready() -> void:
+	animated_sprite_2d.play("default")
+	
 func _on_body_entered(body):
 	if body is Player:
 		body.go_to_next_level()
